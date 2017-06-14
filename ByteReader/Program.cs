e94616b07d502c010f0188b72f7417fb20992f58
JuanFerrer <juanemilioferrer@outlook.com>
2017-06-14 09:15:08 +0100

    First upload

---
+++ ByteReader/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ByteReader
+{
+    public static class ByteReader
+    {
+        public static void PrintBytes(string file)
+        {
+            byte[] bytes = new byte[1];
+            if (File.Exists(file))
+                bytes = GetHexFromFile(file);
+            else
+                Console.WriteLine("File not found");
+            for (int i = 0; i < bytes.Length; ++i)
+                Console.Write(bytes[i].ToString("X2") + " ");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="signature"></param>
+        /// <returns></returns>
+        public static bool CheckFileSignature(string file, string signature)
+        {
+            List<byte> signBytes = new List<byte>(System.Text.Encoding.ASCII.GetBytes(signature));
+            List<byte> fileBytes = new List<byte>(GetHexFromFile(file));
+
+            // Check whether signBytes is a subset of fileBytes
+            return !signBytes.Except(fileBytes).Any();
+        }
+
+        public static byte[] GetHexFromFile(string file)
+        {
+            BinaryReader reader = new BinaryReader(new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None));
+            reader.BaseStream.Position = 0x0;     // The offset you are reading the data from
+            byte[] data = ReadAllBytes(reader);
+            reader.Close();
+            return data;
+        }
+
+
+        public static byte[] ReadAllBytes(BinaryReader reader)
+        {
+            const int bufferSize = 4096;
+            using (var ms = new MemoryStream())
+            {
+                byte[] buffer = new byte[bufferSize];
+                int count;
+                while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+                    ms.Write(buffer, 0, count);
+                return ms.ToArray();
+            }
+        }
+
+        public static void ByteArrayToFile(string fileName, byte[] byteArray)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                fs.Write(byteArray, 0, byteArray.Length);
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string funct = args[0];
+            string file = args[1];
+            if (File.Exists(file))
+            {
+                switch (funct)
+                {
+                    case "-s":
+                        if (ByteReader.ByteReader.CheckFileSignature(file, args[2]))
+                        {
+                            Console.WriteLine("Signature " + args[2] + " was found.");
+                        }
+                        break;
+                    case "-l":
+                        ByteReader.ByteReader.PrintBytes(file);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
